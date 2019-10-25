c9a1bbb17cee2fa65dce6a7445746edde36aca8a
Samuel Matheus <50873803+samuel-matheus@users.noreply.github.com>
2019-10-25 14:30:42 -0300

    Update Frete.java

--- Frete.java
+++ Frete.java
@@ -2,241 +2,109 @@ import java.util.ArrayList;
 import java.util.List;
 
 abstract class Frete {
-    private double distanciaKm, valorPorKm;
- public Frete(double distanciaKm, double valorPorKm){
-     this.distanciaKm = distanciaKm;
-     this.valorPorKm = valorPorKm;
-     }
-     
-     public String toString(){
-      return (" Distancia em Km: ") + distanciaKm + (" Valor Por Km: ") + valorPorKm;
+  private double distanciakm;
+  private double valorPorkm; 
+
+  public Frete(double distanciakm, double valorPorkm){
+    this.distanciakm = distanciakm;
+    this.valorPorkm = valorPorkm;
   }
-     public double getDistanciaKm(){
-         return distanciaKm;
-         
-     } public double getValorPorKm(){
-         return valorPorKm;
-         
-     }
- public abstract double calcularFrete();
-   
+  public double getDistanciakm(){
+    return distanciakm;
+  }
+  public double getValorPorkm(){
+    return valorPorkm;
+  }
+  
+
+  public abstract double calcularFrete();
 }
-class fretePadrao extends Frete {
- public fretePadrao(double distanciaKm,double valorPorKm){
-     super(distanciaKm, valorPorKm);
-     
- }
- public String toString(){
-      return super.toString();
- }
- 
- public double calcularFrete(){
-     return (getDistanciaKm() * getValorPorKm());
-     
- }
+
+class FretePadrao extends Frete {
+
+  public FretePadrao(double distanciakm,double valorPorkm){
+    super(distanciakm, valorPorkm);
+  }
+  public double calcularFrete(){
+    return (getDistanciakm() * getValorPorkm());
+  }
+
 }
+
 class FreteExpresso extends Frete {
-    private int nivelUrgencia;
- public FreteExpresso(double distanciaKm, double valorPorKm, int nivelUrgencia){
-     super(distanciaKm, valorPorKm);
-     this.nivelUrgencia = nivelUrgencia;
-     
- }
-  public String toString(){
-      return super.toString() + "Nivel de Urgencia" + nivelUrgencia;
- }
- 
- public int getNivelUrgencia(){
-     return nivelUrgencia;
-     
- } public double calcularFrete(){
-     return (getDistanciaKm() * getValorPorKm()) + (getNivelUrgencia() * 100);
-     
- }
-   
+  private int nivelUrgencia;
+
+  public FreteExpresso(double distanciakm, double valorPorkm, int nivelUrgencia){
+    super(distanciakm, valorPorkm);
+    this.nivelUrgencia = nivelUrgencia;
+  }
+  public int getNivelUrgencia(){
+    return nivelUrgencia;
+  }
+  public double calcularFrete(){
+    return (getDistanciakm() * getValorPorkm()) + (getNivelUrgencia() * 100);
+  }
 }
+
 class FreteSuperExpresso extends FreteExpresso{
-    private double valorDoSeguro;
- public FreteSuperExpresso(double distanciaKm, double valorPorKm, int nivelUrgencia, double valorDoSeguro){
-     super(distanciaKm, valorPorKm, nivelUrgencia);
-     this.valorDoSeguro = valorDoSeguro;
-     }
-     public String toString(){
-      return super.toString() + " Valor do Seguro: " + valorDoSeguro;
- }
-     
-     public double getValorDoSeguro(){
-         return valorDoSeguro;
-         
-     }
-     public double calcularFrete() {
-         return (getDistanciaKm() * getValorPorKm()) + (getNivelUrgencia() * 100) + getValorDoSeguro();
-        }
-   
+  private double valorDoSeguro;
+
+  public FreteSuperExpresso(double distanciakm, double valorPorkm, int nivelUrgencia, double valorDoSeguro){
+    super(distanciakm, valorPorkm, nivelUrgencia);
+    this.valorDoSeguro = valorDoSeguro;
+  }
+  public double getValorDoSeguro(){
+    return valorDoSeguro;
+  }
+  public double calcularFrete() {
+    return (getDistanciakm() * getValorPorkm()) + (getNivelUrgencia() * 100) + getValorDoSeguro();
+  }
+  
 }
-class CadastroFrete {
-    private int qtd;
-    private List<Frete> frete;
-   
-    public CadastroFrete(int qtd){
-        this.qtd = 0;
-        this.frete = new ArrayList<Frete>();
+
+class CadastroFrete  {
+  private int qtd;
+  private List<Frete> fretes = new ArrayList<Frete>();
+  public CadastroFrete(int qtd){
+    this.qtd = 0; 
+  }
+  public boolean addFrete(Frete f){
+    if (qtd > 10) return false;
+
+    fretes.add(f);
+
+     return true;
+  }
+
+  public double calcularFrete(){
+    double total = 0;
+    for (int i = 0; i < fretes.size(); ++i){
+     if (fretes.get(i) != null) {
+       total = total + fretes.get(i).calcularFrete();
+     }
     }
-    public boolean addFrete( Frete f){
-        this.frete.add(f);
-        return true;
-    }
-    public double calcularFrete(){
-        double total = 0;
-        for (int i = 0; i < frete.size(); ++i){
-            total = total + frete.get(i).calcularFrete(); //polimofismo } }
-           
-        }
-          return total;  
-        }
-       
-    }
+
+    return total;
+
+}
+}
+
 
 public class Main{
-    public static void main(String []args){
-        CadastroFrete c = new CadastroFrete(10);
-        fretePadrao f = new fretePadrao(50.0 , 10.0);
-        FreteExpresso E = new FreteExpresso(50.0 , 10.0, 2);
-        FreteSuperExpresso SE = new FreteSuperExpresso(50.0 , 10.0, 2, 100 );
-        c.addFrete(f);
-        c.addFrete(E);
-        c.addFrete(f);
-        System.out.println(c.calcularFrete());
-    }
-}
+  public static void main(String []args){
+    CadastroFrete c = new CadastroFrete(10);
+    FretePadrao f = new FretePadrao(100.0, 15.0);
+    FreteExpresso e = new FreteExpresso(100.0, 30.0, 1);
+    FreteSuperExpresso s = new FreteSuperExpresso(100.0, 20.0, 2, 50);
 
+    c.addFrete(f);
+    c.addFrete(e);
+    c.addFrete(s);
+    System.out.println(c.calcularFrete());
 
-MILITAR 2.0
-import java.util.ArrayList;
-import java.util.List;
-
-abstract class Militar {
-  private String patente;
-  private int matricula;
- 
-
-  public Militar(String patente, int matricula){
-    this.patente = patente;
-    this.matricula = matricula;
+    System.out.println("O Valor do frete padrão é: " + f.calcularFrete() + "R$ ");
+    System.out.println("O Valor do frete Expresso é: " + e.calcularFrete() + "R$ ");
+    System.out.println("O Valor do frete Super Expresso é: " + s.calcularFrete()+ "R$ " );
+    
   }
- 
-  public String toString(){
-      return ("Patente: ") + patente + ("Matricula: ") + matricula;
-  }
-  public String getpatente(){
-    return patente;
-  }
-  public int getMatricula(){
-    return matricula;
-  }
- 
-
- public abstract boolean podeProgredir();
- 
-}
-
-
-class MilitarAeronautica extends Militar{
-  private double duraPatente;
-  private double horaVoo;
-
-  public MilitarAeronautica(double duraPatente, double horaVoo,String patente, int matricula){
-    super (patente, matricula);
-    this.duraPatente = duraPatente;
-    this.horaVoo = horaVoo;
-  }
- 
-  public String toString(){
-      return  super.toString() + " Duração da Patente: " + duraPatente + " horas Voando: " + horaVoo;
-  }
-  public boolean podeProgredir(){
-    if (duraPatente > 2 && horaVoo > 100){
-      return true;
-    }else {
-      return false;
-    }
-  }
-}
-
-class MilitarExercito extends Militar {
-  private boolean ptcp, pgg;
-  public MilitarExercito(String patente, int matricula, boolean ptcp, boolean pgg){
-     super(patente,matricula);
-     this.ptcp = ptcp;
-     this.pgg = pgg;
-  }
- 
-  public String toString(){
-      return  super.toString() + " Participou da Guerra: " + ptcp + " Ganhou a Guerra: " + pgg;
-  }
- 
-  public boolean podeProgredir(){
-    if (ptcp == true && pgg == true){
-      return true;
-    }else{
-      return false;
-    }
-  }
-}
-
-class MilitarMarinha extends Militar {
-  private boolean pceam;
-  public MilitarMarinha(String patente, int matricula, boolean pceam){
-    super(patente,matricula);
-    this.pceam = pceam;
-  }
- 
-  public String toString(){
-      return  super.toString() + " Consertou um avião em alto-mar: " + pceam;
-  }
- 
-  public boolean podeProgredir(){
-    if (pceam == true){
-      return true;
-    }else{
-      return false;
-    }
-  }
-}
-
-class CadastroMilitar{
-  private int qtd;
-  private List<Militar> militar;
-  public CadastroMilitar(int qtd){
-    this.qtd = 0;
-    this.militar =  new ArrayList<Militar>();
-}
-
-  public boolean addMilitar( Militar m){
-   
-      this.militar.add(m);
-      return true;
-  }
- 
-  public void militarPodeProgredir(){
-    for(int i = 0; i < militar.size();   ++i){
-        if(militar.get(i).podeProgredir() == true){
-          System.out.println(militar.get(i));
-        }
-    }
-  }
-}
-
-public class Main {
-    public static void main(String[] args) {
-    CadastroMilitar c = new CadastroMilitar(10);
-    MilitarAeronautica Ma = new MilitarAeronautica(15.8, 100.5, "Capitao",15546575);
-    MilitarExercito Me = new MilitarExercito( "Capitao",15546575, true, false);
-    MilitarMarinha Mm = new MilitarMarinha( "Capitao",15546575, true);
-    c.addMilitar(Ma);
-    c.addMilitar(Mm);
-    c.addMilitar(Me);
-    c.militarPodeProgredir();
-       
-    }
 }
